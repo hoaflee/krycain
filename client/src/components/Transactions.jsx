a42261940425a4539824bbb4bfde1ad97c42d73f
hoaflee <lehoanghoa1008@gmail.com>
2022-02-05 23:06:17 +0900

    fix layour

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { RiH1 } from "react-icons/ri";
 import { TransactionContext } from "../context/TransactionContext";
 import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
@@ -12,7 +13,31 @@ const TransactionCard = ({
   amount,
   url,
 }) => {
-  return "transaction";
+  return (
+    <div
+      className="bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-col p-3 rounded-md hover:shadow-2xl
+    "
+    >
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="flex  justify-start w-full mb-6 p-2">
+          <a
+            href={`https://ropsten.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base">
+              From: {shortenAddress(addressFrom)}
+            </p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const Transactions = () => {
@@ -23,20 +48,18 @@ const Transactions = () => {
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
-            {" "}
             Lastest Transactions
           </h3>
         ) : (
           <h3 className="text-white text-3xl text-center my-2">
-            {" "}
             Connect your account
           </h3>
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => {
-            <TransactionCard key={i} {...transaction} />;
-          })}
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
         </div>
       </div>
     </div>
